225c0024975ce81366daa83e28951fae026ad402
elghazouanifati <elghazouani4b@gmail.com>
2026-03-09 23:01:22 +0100

    design

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-
+import { Navigation } from "./components/Navigation";
+import { Hero } from "./components/Hero";
+import { About } from "./components/About";
+import { Skills } from "./components/Skills";
+import { Projects } from "./components/Projects";
+import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 
 export default function App() {
   return (
-    <>
     <div className="min-h-screen bg-white">
-      welcome to the 1st version of Fati's portfolio! I'm a software engineer with a passion for building innovative and efficient solutions. With experience in various programming languages and frameworks, I enjoy tackling complex problems and creating impactful applications. Feel free to explore my projects and get in touch if you'd like to collaborate or learn more about my work.
+      <Navigation />
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
-    </>
   );
 }
