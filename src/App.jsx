eca8116538839a296d3a430e7f13424cf6bcb09b
elghazouanifati <elghazouani4b@gmail.com>
2026-03-09 21:57:24 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+export default function App() {
+  return (
+    <>
+    <div className="min-h-screen bg-white">
+      welcome to the 1st version of Fati's portfolio! I'm a software engineer with a passion for building innovative and efficient solutions. With experience in various programming languages and frameworks, I enjoy tackling complex problems and creating impactful applications. Feel free to explore my projects and get in touch if you'd like to collaborate or learn more about my work.
+    </div>
+    </>
+  );
+}
